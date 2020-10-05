a9a46224f68966b535994437a26bcd1558bf75ee
vishal <vishalbansode03.vb@gmail.com>
2020-10-05 22:25:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title="vishal";
+  teams$: Observable<any> = this.http.get('/api/teams');
+  myjson= {
+    "id": 3,
+    "name": "vishal bansode",
+    "coach": "aurangabad",
+    "description": "k!"
+  }
+​
+  constructor(private http: HttpClient) {}
 }
